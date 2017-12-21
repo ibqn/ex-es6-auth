041f255e4e4e58a867ab3ae8192bc2b31a2261e2
Evgeny Bobkin <evgen.ibqn@gmail.com>
2017-12-21 06:50:02 +0100

    remove useless comments. join user and fb tables

--- src/strategy/local.js
+++ src/strategy/local.js
@@ -1,18 +1,18 @@
 import LocalStrategy from 'passport-local'
 import { User } from '../models/user'
+import { Facebook } from '../models/facebook'
 
 
 export const initLocalStrategy = (passport) => {
-  // used to serialize the user for the session
   passport.serializeUser(async (user, done) => {
     done(null, user.id)
   })
 
-  // used to deserialize the user
   passport.deserializeUser(async (id, done) => {
     try {
       const user = await User.findOne({
         where: { id },
+        include: [ Facebook ],
         attributes: [ 'id', 'email', 'password' ]
       })
       done(null, user)
@@ -22,58 +22,43 @@ export const initLocalStrategy = (passport) => {
   })
 
   passport.use('local-login', new LocalStrategy({
-    // by default, local strategy uses username and password, we will override with email
     usernameField: 'email',
     passwordField: 'password',
-    // allows us to pass back the entire request to the callback
     passReqToCallback: true
   },
-  // callback with email and password from our form
   async function(req, email, password, done) {
     try {
-      // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
       const user = await User.findOne({
         where: { email },
         attributes: [ 'id', 'email', 'password', 'salt' ]
       })
-      // if no user is found, return the message
       if (user === null) {
-        // req.flash is the way to set flashdata using connect-flash
         return done(
           null,
           false,
           req.flash('loginMessage', 'No user found.')
         )
       }
-      // if the user is found but the password is wrong
       if (!await user.validPassword(password)) {
-        // create the loginMessage and save it to session as flashdata
         return done(
           null,
           false,
           req.flash('loginMessage', 'Oops! Wrong password.')
         )
       }
-      // all is well, return successful user
       return done(null, user)
     } catch (error) {
-      // if there are any errors, return the error before anything else
       return done(error)
     }
   }))
 
   passport.use('local-signup', new LocalStrategy({
-    // by default, local strategy uses username and password, we will override with email
     usernameField: 'email',
     passwordField: 'password',
-    // allows us to pass back the entire request to the callback
     passReqToCallback: true
   },
   async function(req, email, password, done) {
     try {
-      // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
       let user = await User.findOne({
         where: { email },
         attributes: [ 'id', 'email' ]
@@ -85,7 +70,6 @@ export const initLocalStrategy = (passport) => {
           req.flash('signupMessage', 'That email is already taken.')
         )
       } else {
-        // if there is no user with that email, create the user
         user = await User.build({ email })
         await user.setPassword(password)
         await user.save()
